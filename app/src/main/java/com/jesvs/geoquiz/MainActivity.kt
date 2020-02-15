43986b7af3a2f6dfcb00a0e61ece2147a02f836e
Jesús Sánchez <ahinosvemos@gmal.com>
2020-02-15 12:06:44 -0700

    Chapter 3

--- app/src/main/java/com/jesvs/geoquiz/MainActivity.kt
+++ app/src/main/java/com/jesvs/geoquiz/MainActivity.kt
@@ -2,12 +2,15 @@ package com.jesvs.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.StringRes
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,10 +29,13 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_oceans, true)
     )
 
+    private val responseBank = mutableListOf<Boolean?>(null, null, null, null, null, null)
+
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -40,10 +46,14 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener {
             checkAnswer(true)
+            checkResult()
+            updateAnswerButtons()
         }
 
         falseButton.setOnClickListener {
             checkAnswer(false)
+            checkResult()
+            updateAnswerButtons()
         }
 
         nextButton.setOnClickListener {
@@ -59,11 +69,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         updateQuestion()
+        updateAnswerButtons()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun nextQuestion() {
         currentIndex = (currentIndex + 1) % questionBank.size
         updateQuestion()
+        updateAnswerButtons()
     }
 
     private fun prevQuestion() {
@@ -72,6 +109,7 @@ class MainActivity : AppCompatActivity() {
             currentIndex = questionBank.size - 1
         }
         updateQuestion()
+        updateAnswerButtons()
     }
 
     private fun updateQuestion() {
@@ -79,7 +117,14 @@ class MainActivity : AppCompatActivity() {
         questionTextView.setText(questionTextResId)
     }
 
+    private fun updateAnswerButtons() {
+        val unanswered = responseBank[currentIndex] == null
+        trueButton.isEnabled = unanswered
+        falseButton.isEnabled = unanswered
+    }
+
     private fun checkAnswer(userAnswer: Boolean) {
+        responseBank[currentIndex] = userAnswer
         val correctAnswer = questionBank[currentIndex].answer
 
         val messageResId = if (userAnswer == correctAnswer) {
@@ -90,8 +135,27 @@ class MainActivity : AppCompatActivity() {
         showToast(messageResId)
     }
 
+    private fun checkResult() {
+        var score = 0
+        for ((i, r) in responseBank.withIndex()) {
+            if (r == null) {
+                return
+            }
+            if (questionBank[i].answer == r) {
+                score++
+            }
+        }
+
+        val total = questionBank.size
+        val percentage = score / total.toFloat() * 100
+
+        Toast.makeText(this, "You answered $score right out of ${total}. Your score is ${percentage} %.", Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.TOP, 0, 200)
+        }.show()
+    }
+
     private fun showToast(@StringRes resId: Int) {
-        Toast.makeText(this, resId, Toast.LENGTH_SHORT).apply {
+        Toast.makeText(this, resId, Toast.LENGTH_LONG).apply {
             setGravity(Gravity.TOP, 0, 200)
         }.show()
     }
