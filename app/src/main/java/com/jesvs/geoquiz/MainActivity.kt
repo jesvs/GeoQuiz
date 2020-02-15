784e77dc73c7c67c3c7a969b307c949f0479a583
Jesús Sánchez <ahinosvemos@gmal.com>
2020-02-14 18:32:10 -0700

    Chapter 1

--- app/src/main/java/com/jesvs/geoquiz/MainActivity.kt
+++ app/src/main/java/com/jesvs/geoquiz/MainActivity.kt
@@ -2,11 +2,34 @@ package com.jesvs.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            showToast(R.string.correct_toast)
+        }
+
+        falseButton.setOnClickListener {
+            showToast(R.string.incorrect_toast)
+        }
+    }
+
+    private fun showToast(resId: Int) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.TOP, 0, 0)
+        }.show()
     }
 }
